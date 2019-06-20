a3e51c879b1e160969740e61f45254188286dfa1
20181105879 <2605753145@qq.com>
2019-06-20 10:21:38 +0800

    Initial Commit

---
+++ 19.06.20.01/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  19.06.20.01
+//
+//  Created by s20181105879 on 2019/6/20.
+//  Copyright © 2019 s20181105879. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
